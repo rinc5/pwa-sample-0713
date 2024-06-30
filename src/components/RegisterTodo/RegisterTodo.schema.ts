96e6f84b49b877a942722893bf0879aa5381dfbc
azufb <curiousazunas@gmail.com>
2024-06-30 17:00:06 +0900

    バリデーションエラーメッセージ定義

--- src/components/RegisterTodo/RegisterTodo.schema.ts
+++ src/components/RegisterTodo/RegisterTodo.schema.ts
@@ -2,7 +2,10 @@ import { z } from "zod";
 
 export const RegisterTodoSchema = z
   .object({
-    title: z.string().min(1).max(20),
+    title: z
+      .string()
+      .min(1, "必須入力項目です")
+      .max(20, "20文字以内で入力してください"),
   })
   .required({
     title: true,
