a6c4e658ae27a71a232b037b22530b12ca335f87
Asumi Iwamoto <BF066601@jp.ibm.com>
2024-07-13 15:35:15 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# 20240713 TECH_WOMAN_KANSAI PWA サンプル
+
+TECH WOMAN KANSAI のイベントで使用するサンプルコードです！<br>
+ToDo 管理アプリです。
+
+## 機能
+
+- ToDo 機能
+  - 追加
+  - 一覧表示
+  - 削除（個別・一括）
+- Push 通知
+
+## 技術スタック
+
+### フロント
+
+- TypeScript
+- React
+- TailwindCSS
+- React Hook Form
+- Zod
+
+### ストレージ
+
+- LocalStorage
+  - オフラインでデータ取得可能にするため
+  - LocalStorage は認知度としては高いのと、手軽に扱えるため
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { RegisterTodo } from "./components/organisms/RegisterTodo/RegisterTodo";
+import { TodoList } from "./components/organisms/TodoList/TodoList";
+import { TodoItem } from "./components/organisms/TodoList/TodoListPresenter";
+import { LOCAL_STORAGE_NAME } from "./CONST";
+
+function App() {
+  const todos = localStorage.getItem(LOCAL_STORAGE_NAME);
+  const parsedTodos = todos != null ? JSON.parse(todos) : [];
+  const [todoList, setTodoList] = useState<TodoItem[]>(parsedTodos);
+
+  return (
+    <div className="px-4">
+      <h1 className="text-center my-6 font-bold text-3xl">PWA TODO SAMPLE</h1>
+      {/** Todo登録フォーム */}
+      <RegisterTodo setTodoList={setTodoList} />
+
+      <hr className="my-4" />
+
+      {/** Todo一覧 */}
+      <TodoList todoList={todoList} setTodoList={setTodoList} />
+    </div>
+  );
+}
+
+export default App;
